8c68a57cd50f7610d2164ce934d8e63a7c9d9e82
isachenkoff <isachenkoff.alexander@yandex.ru>
2023-07-03 12:48:36 +0300

    serialized objects client

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Имя: ");
+        String name = scanner.nextLine();
+
+        Client client = new Client();
+
+        System.out.println("Сообщения:");
+        while (true) {
+            String s = scanner.nextLine();
+            TextMessage message = new TextMessage(name, s);
+            client.postTextMessage(message);
+        }
+
     }
 
 }
