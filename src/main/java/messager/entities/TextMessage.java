d60b84a0183799ca1d51e8a242167db62b1bf877
isachenkoff <isachenkoff.alexander@yandex.ru>
2023-07-07 13:18:59 +0300

    lombok

--- src/main/java/messager/entities/TextMessage.java
+++ src/main/java/messager/entities/TextMessage.java
@@ -1,10 +1,17 @@
 package messager.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import java.io.Serializable;
 
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 @XmlRootElement
 public class TextMessage implements Serializable {
 
@@ -14,31 +21,5 @@ public class TextMessage implements Serializable {
     private User userTo;
     @XmlAttribute
     private String message;
-
-    public TextMessage() {
-    }
-
-    public TextMessage(User userFrom, User userTo, String message) {
-        this.userFrom = userFrom;
-        this.userTo = userTo;
-        this.message = message;
-    }
-
-    public User getUserFrom() {
-        return userFrom;
-    }
-
-    public User getUserTo() {
-        return userTo;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    @Override
-    public String toString() {
-        return String.format("%s: %s", getUserFrom().getName(), message);
-    }
 
 }
--- src/main/java/messager/entities/User.java
+++ src/main/java/messager/entities/User.java
@@ -1,8 +1,15 @@
 package messager.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlRootElement;
 
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 @XmlRootElement
 public class User {
 
@@ -10,22 +17,5 @@ public class User {
     private String name;
     @XmlAttribute
     private String password;
-
-    public User() {
-    }
-
-    public User(String name, String password) {
-        this.name = name;
-        this.password = password;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-
-    public String getPassword() {
-        return password;
-    }
 
 }
