f20f5f13c782e3a982b117be9cff6b583986354f
isachenkoff <isachenkoff.alexander@yandex.ru>
2023-07-07 16:36:45 +0300

    selectable message text

--- src/main/java/messager/controller/MessageCellController.java
+++ src/main/java/messager/controller/MessageCellController.java
@@ -2,6 +2,7 @@ package messager.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import messager.entities.TextMessage;
 
 import java.time.LocalDateTime;
@@ -10,17 +11,24 @@ import java.time.format.FormatStyle;
 
 public class MessageCellController {
     @FXML
+    private Label textLabel;
+    @FXML
     private Label userNameLabel;
     @FXML
-    private Label messageTextLabel;
+    private TextArea messageTextArea;
     @FXML
     private Label timeLabel;
 
     private static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT);
 
+    @FXML
+    private void initialize() {
+        textLabel.textProperty().bind(messageTextArea.textProperty());
+    }
+
     public void setTextMessage(TextMessage textMessage) {
         userNameLabel.setText(textMessage.getUserFrom().getName());
-        messageTextLabel.setText(textMessage.getMessage());
+        messageTextArea.setText(textMessage.getMessage());
         LocalDateTime time = LocalDateTime.parse(textMessage.getDateTime());
         String format = time.format(TIME_FORMATTER);
         timeLabel.setText(format);
