c2be9c069446591953457b5bafe2440eadc03000
isachenkoff <isachenkoff.alexander@yandex.ru>
2023-07-27 14:59:29 +0300

    sign in loading indicator

--- src/main/java/messager/controller/SignInController.java
+++ src/main/java/messager/controller/SignInController.java
@@ -2,7 +2,9 @@ package messager.controller;
 
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ProgressIndicator;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
@@ -13,10 +15,13 @@ import messager.requests.TransferableObject;
 import messager.response.SignInResponse;
 import messager.view.AlertUtil;
 
+import java.net.ConnectException;
 import java.net.SocketTimeoutException;
 
 public class SignInController {
 
+    public ProgressIndicator progress;
+    public Button signInButton;
     @FXML
     private TextField nameField;
     @FXML
@@ -26,6 +31,7 @@ public class SignInController {
 
     @FXML
     private void initialize() {
+        setLoadingState(false);
         responseLabel.setText("");
         nameField.setOnKeyPressed(this::onEnterPressed);
         passwordField.setOnKeyPressed(this::onEnterPressed);
@@ -42,24 +48,35 @@ public class SignInController {
         params.put("userName", nameField.getText());
         params.put("password", passwordField.getText());
 
+        setLoadingState(true);
+
         Request request = new Request("signIn", params);
         ClientServer.instance().postAndAccept(request, SignInResponse.class,
-                response -> Platform.runLater(() -> processSignInResponse(response)),
+                response -> Platform.runLater(() -> {
+                    processSignInResponse(response);
+                    setLoadingState(false);
+                }),
                 e -> {
-                    if (e instanceof SocketTimeoutException) {
-                        responseLabel.setText("Превышено время ожидания подключения к серверу!");
-                    } else {
-                        e.printStackTrace();
-                        AlertUtil.showErrorAlert(e.getMessage());
-                    }
+                    Platform.runLater(() -> {
+                        if (e instanceof SocketTimeoutException || e instanceof ConnectException) {
+                            responseLabel.setText("Превышено время ожидания подключения к серверу!");
+                        } else {
+                            e.printStackTrace();
+                            AlertUtil.showErrorAlert(e.getMessage());
+                        }
+                        setLoadingState(false);
+                    });
                 },
                 e -> {
-                    if (e instanceof SocketTimeoutException) {
-                        responseLabel.setText("Превышено время ожидания ответа от сервера!");
-                    } else {
-                        e.printStackTrace();
-                        AlertUtil.showErrorAlert(e.getMessage());
-                    }
+                    Platform.runLater(() -> {
+                        if (e instanceof SocketTimeoutException) {
+                            responseLabel.setText("Превышено время ожидания ответа от сервера!");
+                        } else {
+                            e.printStackTrace();
+                            AlertUtil.showErrorAlert(e.getMessage());
+                        }
+                        setLoadingState(false);
+                    });
                 });
     }
 
@@ -75,6 +92,14 @@ public class SignInController {
                 responseLabel.setText(String.format("Не зарегистрирован пользователь \"%s\"", nameField.getText()));
                 break;
         }
+    }
+
+    private void setLoadingState(boolean isLoading) {
+        progress.setVisible(isLoading);
+        progress.setManaged(isLoading);
+        responseLabel.setVisible(!isLoading);
+        responseLabel.setManaged(!isLoading);
+        signInButton.setDisable(isLoading);
     }
 
     private void signIn(User user) {
